314be0b259806c1538a0a966578b6f7a42a20e2f
Florian Regente <Florian@Florians-MBP-2.fritz.box>
2015-06-28 14:08:05 +0200

    first

---
+++ jenkins/jenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jenkins
+//
+//  Created by Florian Regente on 28.06.15.
+//  Copyright (c) 2015 Florian Regente. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jenkins/jenkins/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jenkins
+//
+//  Created by Florian Regente on 28.06.15.
+//  Copyright (c) 2015 Florian Regente. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
